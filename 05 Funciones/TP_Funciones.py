957a67333eb53c31681161f7e8ef34e5140e634e
chivax <chivax@gmail.com>
2025-05-29 19:19:12 -0300

    Agregado TP Funciones

---
+++ 05 Funciones/TP_Funciones.py
@@ -0,0 +1,46 @@
+# Actividades:
+# 1. Crear una función llamada imprimir_hola_mundo que imprima por
+# pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+# programa principal.
+
+def imprimir_hola_mundo():
+    print("Hola Mundo!")    
+
+# Llamada a la función
+imprimir_hola_mundo()
+
+    
+
+
+# 2. Crear una función llamada saludar_usuario(nombre) que reciba
+# como parámetro un nombre y devuelva un saludo personalizado.
+# Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá de￾volver: “Hola Marcos!”. Llamar a esta función desde el programa
+# principal solicitando el nombre al usuario.
+# 3. Crear una función llamada informacion_personal(nombre, apellido,
+# edad, residencia) que reciba cuatro parámetros e imprima: “Soy
+# [nombre] [apellido], tengo [edad] años y vivo en [residencia]”. Pe￾dir los datos al usuario y llamar a esta función con los valores in￾gresados.
+# 1
+# Programación I
+# TECNICATURA UNIVERSITARIA
+# EN PROGRAMACIÓN
+# A DISTANCIA
+# 4. Crear dos funciones: calcular_area_circulo(radio) que reciba el ra￾dio como parámetro y devuelva el área del círculo. calcular_peri￾metro_circulo(radio) que reciba el radio como parámetro y devuel￾va el perímetro del círculo. Solicitar el radio al usuario y llamar am￾bas funciones para mostrar los resultados.
+# 5. Crear una función llamada segundos_a_horas(segundos) que reciba
+# una cantidad de segundos como parámetro y devuelva la cantidad
+# de horas correspondientes. Solicitar al usuario los segundos y mos￾trar el resultado usando esta función.
+# 6. Crear una función llamada tabla_multiplicar(numero) que reciba un
+# número como parámetro y imprima la tabla de multiplicar de ese
+# número del 1 al 10. Pedir al usuario el número y llamar a la fun￾ción.
+# 7. Crear una función llamada operaciones_basicas(a, b) que reciba
+# dos números como parámetros y devuelva una tupla con el resulta￾do de sumarlos, restarlos, multiplicarlos y dividirlos. Mostrar los re￾sultados de forma clara.
+# 8. Crear una función llamada calcular_imc(peso, altura) que reciba el
+# peso en kilogramos y la altura en metros, y devuelva el índice de
+# masa corporal (IMC). Solicitar al usuario los datos y llamar a la fun￾ción para mostrar el resultado con dos decimales.
+# 9. Crear una función llamada celsius_a_fahrenheit(celsius) que reciba
+# una temperatura en grados Celsius y devuelva su equivalente en
+# Fahrenheit. Pedir al usuario la temperatura en Celsius y mostrar el
+# resultado usando la función.
+# 10.Crear una función llamada calcular_promedio(a, b, c) que reciba
+# tres números como parámetros y devuelva el promedio de ellos.
+# Solicitar los números al usuario y mostrar el resultado usando esta
+# función.
